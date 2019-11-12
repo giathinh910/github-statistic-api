75dce23b9e625382a2790140add6cca78b1e895a
Bui Gia Thinh <bui.gia.thinh@sun-asterisk.com>
2019-11-12 13:31:56 +0700

    remove redundant parts

--- app.js
+++ app.js
@@ -1,58 +1,29 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
-const session = require('express-session');
 const cors = require('cors');
 require('dotenv').config();
-const {
-  isAuthenticated,
-  isNotAuthenticated,
-} = require('./middlewares');
-
-const indexRouter = require('./routes/index');
-const loginRouter = require('./routes/login');
-const logoutRouter = require('./routes/logout');
-const oauth2Router = require('./routes/oauth2');
 const oauthApi = require('./api/oauth');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
+
 app.use(express.json());
+
 app.use(express.urlencoded({
   extended: false,
 }));
-app.use(cookieParser());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  cookie: {
-    secure: false,
-    maxAge: 60 * 60 * 1000,
-  },
-  resave: false,
-  saveUninitialized: true,
-}));
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true,
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('/', isAuthenticated, indexRouter);
-app.use('/login', isNotAuthenticated, loginRouter);
-app.use('/logout', isAuthenticated, logoutRouter);
-app.use('/auth', isNotAuthenticated, oauth2Router);
+app.use(cookieParser());
+
+app.use('/', function(req, res, next) {
+  res.send('Hello World!')
+});
 
 app.use(cors());
+
 app.use('/api/auth', oauthApi);
 
 // catch 404 and forward to error handler
--- routes/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res, next) {
-  req.session.destroy();
-  res.redirect('/login');
-});
-
-module.exports = router;
